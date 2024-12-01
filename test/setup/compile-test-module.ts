884c93a73fd53dde5bd69f73199236a1fdeb8a0d
Joren Vandeweyer <joren.vandeweyer@appwise.be>
2024-12-01 12:59:27 +0100

    fix test setup

--- test/setup/compile-test-module.ts
+++ test/setup/compile-test-module.ts
@@ -1,8 +1,13 @@
 import { Test, type TestingModule } from '@nestjs/testing'
 import { AppModule } from '../../src/app.module.js'
+import { TwilioService } from '../../src/modules/twilio/twilio.service.js'
+import { TwilioMockService } from '../../src/modules/twilio/twilio.service.mock.js'
 
 export async function compileTestModule (): Promise<TestingModule> {
   const imports = [AppModule.forRoot([], true)]
 
-  return await Test.createTestingModule({ imports }).compile()
+  return await Test.createTestingModule({ imports })
+    .overrideProvider(TwilioService)
+    .useClass(TwilioMockService)
+    .compile()
 }
