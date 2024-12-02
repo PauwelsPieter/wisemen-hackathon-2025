54fe188ccca4577f89adbd7b603abcef11355b4a
Joren Vandeweyer <joren.vandeweyer@appwise.be>
2024-12-02 12:56:36 +0100

    update migration

--- src/config/sql/migrations/1732990798611-EnablePgBossWorker.ts
+++ src/config/sql/migrations/1732990798611-EnablePgBossWorker.ts
@@ -2,7 +2,18 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class EnablePgBossWorker1732990798611 implements MigrationInterface {
   public async up (queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER SCHEMA pgboss RENAME TO pgboss_v9`)
+    await queryRunner.query(`
+      DO $$
+      BEGIN
+        IF EXISTS (
+          SELECT 1
+          FROM information_schema.schemata
+          WHERE schema_name = 'pgboss'
+        ) THEN
+          EXECUTE 'ALTER SCHEMA pgboss RENAME TO pgboss_v9';
+        END IF;
+      END $$;
+    `)
 
     await queryRunner.query(`SELECT pgboss.create_queue('system', '{"policy":"stately"}')`)
     await queryRunner.query(`SELECT pgboss.create_queue('nats', '{"policy":"stately"}')`)
