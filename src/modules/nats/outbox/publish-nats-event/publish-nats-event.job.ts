b979ffc8ec494176b498b0fab944295875d58cf3
Joren Vandeweyer <joren.vandeweyer@appwise.be>
2024-12-09 14:44:04 +0100

    define some options

--- src/modules/nats/outbox/publish-nats-event/publish-nats-event.job.ts
+++ src/modules/nats/outbox/publish-nats-event/publish-nats-event.job.ts
@@ -1,4 +1,16 @@
+import { SECONDS_PER_MINUTE } from '@appwise/time'
 import { BaseJobConfig } from '../../../pgboss/jobs/job.abstract.js'
 import type { NatsOutboxEvent } from '../nats-outbox-event.js'
 
-export class PublishNatsEventJob extends BaseJobConfig<NatsOutboxEvent> {}
+export class PublishNatsEventJob extends BaseJobConfig<NatsOutboxEvent> {
+  constructor (
+    data: NatsOutboxEvent
+  ) {
+    super(data, {
+      priority: 0,
+      retryLimit: 3,
+      retryBackoff: false,
+      expireInSeconds: 5 * SECONDS_PER_MINUTE
+    })
+  }
+}
