4282ea1abda04eb32d85810d1b23abe8b0b19148
Joren Vandeweyer <joren.vandeweyer@appwise.be>
2024-12-19 20:53:13 +0100

    optional

--- src/modules/preferences/use-cases/update-preferences/update-preferences.command.ts
+++ src/modules/preferences/use-cases/update-preferences/update-preferences.command.ts
@@ -1,34 +1,34 @@
 import { IsBoolean, IsEnum, IsOptional, IsString } from 'class-validator'
-import { ApiProperty } from '@nestjs/swagger'
+import { ApiPropertyOptional } from '@nestjs/swagger'
 import { Theme } from '../../types/theme.enum.js'
 
 export class UpdatePreferencesCommand {
-  @ApiProperty({ enum: Theme })
+  @ApiPropertyOptional({ enum: Theme })
   @IsOptional()
   @IsEnum(Theme)
   theme?: Theme
 
-  @ApiProperty({ type: String, example: 'en' })
+  @ApiPropertyOptional({ type: String, example: 'en' })
   @IsOptional()
   @IsString()
   language?: string | null
 
-  @ApiProperty({ type: String, example: 'default' })
+  @ApiPropertyOptional({ type: String, example: 'default' })
   @IsOptional()
   @IsString()
   fontSize?: string | null
 
-  @ApiProperty({ type: Boolean, example: false })
+  @ApiPropertyOptional({ type: Boolean, example: false })
   @IsOptional()
   @IsBoolean()
   showShortcuts?: boolean
 
-  @ApiProperty({ type: Boolean, example: false })
+  @ApiPropertyOptional({ type: Boolean, example: false })
   @IsOptional()
   @IsBoolean()
   reduceMotion?: boolean
 
-  @ApiProperty({ type: Boolean, example: false })
+  @ApiPropertyOptional({ type: Boolean, example: false })
   @IsOptional()
   @IsBoolean()
   highContrast?: boolean
