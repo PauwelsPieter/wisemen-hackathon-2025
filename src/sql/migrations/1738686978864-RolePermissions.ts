4ae0a50e6cdf409bae4bce0cdca2804d2daaad2c
Joren Vandeweyer <joren.vandeweyer@appwise.be>
2025-02-07 10:47:19 +0100

    update role permission seeder

--- src/sql/migrations/1738686978864-RolePermissions.ts
+++ src/sql/migrations/1738686978864-RolePermissions.ts
@@ -1,26 +1,29 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
-import { Roles, rolePermissions } from '../../modules/roles/defaults.roles.js'
+import { Permission } from '../../modules/permission/permission.enum.js'
+
+export const rolePermissions: Record<string, Permission[]> = {
+  admin: [Permission.ALL_PERMISSIONS],
+  readonly: []
+}
 
 export class RolePermissions1738686978864 implements MigrationInterface {
   name = 'RolePermissions1738686978864'
 
   public async up (queryRunner: QueryRunner): Promise<void> {
-    for (const roleName of Object.values(Roles)) {
-      const permissions = rolePermissions[roleName]
-
+    for (const [name, permissions] of Object.entries(rolePermissions)) {
       await queryRunner.query(
-        `INSERT INTO "role" ("name", "permissions") 
+        `INSERT INTO "role" ("name", "permissions")
          VALUES ($1, $2)
-         ON CONFLICT ("name") 
+         ON CONFLICT ("name")
          DO UPDATE SET "permissions" = $2`,
-        [roleName, permissions]
+        [name, permissions]
       )
     }
   }
 
   public async down (queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `DELETE FROM "role" WHERE "name" IN (${Object.values(Roles).map(role => `'${role}'`).join(', ')})`
+      `DELETE FROM "role" WHERE "name" IN (${Object.keys(rolePermissions).map(role => `'${role}'`).join(', ')})`
     )
   }
 }
