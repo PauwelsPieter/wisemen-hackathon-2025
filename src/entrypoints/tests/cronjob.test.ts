06d7a9f6a2898c5fd0b9ea14acc2249aab122568
jonasbeckers <jonas.beckers@wisemen.digital>
2025-04-03 14:31:28 +0200

    Add test for cronjobs (#247)

---
+++ src/entrypoints/tests/cronjob.test.ts
@@ -0,0 +1,26 @@
+import { describe, it } from 'node:test'
+import { Test } from '@nestjs/testing'
+import { expect } from 'expect'
+import { CronjobType } from '../../modules/cronjobs/enums/cronjob-type.enum.js'
+import { AppModule } from '../../app.module.js'
+import { CronjobModule } from '../../modules/cronjobs/cronjob.module.js'
+
+describe('Cronjob tests', () => {
+  async function testCronjobStartup (type: CronjobType): Promise<void> {
+    const cronjobModule = AppModule.forRoot([
+      CronjobModule.forRootAsync({ type })
+    ])
+
+    const worker = await Test.createTestingModule({
+      imports: [cronjobModule]
+    }).compile()
+
+    return worker.close()
+  }
+
+  for (const type of Object.values(CronjobType)) {
+    it(`Cronjob ${type} starts successfully`, async () => {
+      await expect(testCronjobStartup(type)).resolves.not.toThrow()
+    })
+  }
+})
