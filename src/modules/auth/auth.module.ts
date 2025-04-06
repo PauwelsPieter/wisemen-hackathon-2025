27ca6dfdef2b21a29a9ccfb928475f8f079dcc64
Maarten Sijmkens <maarten.sijmkens@gmail.com>
2025-04-06 21:21:01 +0200

    refactor: websocket service module (#254)

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,16 +2,20 @@ import { Global, Module } from '@nestjs/common'
 import { APP_GUARD } from '@nestjs/core'
 import { PermissionModule } from '../permission/permission.module.js'
 import { PermissionsGuard } from '../permission/permission.guard.js'
+import { UserModule } from '../../app/users/user.module.js'
 import { AuthGuard } from './guards/auth.guard.js'
 import { AuthContext } from './auth.context.js'
+import { AuthMiddleware } from './middleware/auth.middleware.js'
 
 @Global()
 @Module({
   imports: [
+    UserModule,
     PermissionModule
   ],
   providers: [
     AuthContext,
+    AuthMiddleware,
     {
       provide: APP_GUARD,
       useClass: AuthGuard
--- src/modules/websocket/websocket.module.ts
+++ src/modules/websocket/websocket.module.ts
@@ -1,21 +1,19 @@
 import { Module } from '@nestjs/common'
-import { NatsClient } from '../nats/nats.client.js'
 import { UserModule } from '../../app/users/user.module.js'
 import { RoleModule } from '../../app/roles/role.module.js'
-import { AuthMiddleware } from '../auth/middleware/auth.middleware.js'
+import { NatsModule } from '../nats/nats.module.js'
 import { WebsocketNatsGateway } from './websocket-nats.gateway.js'
 import { WebsocketTopicValidator } from './websocket-topic.validator.js'
 
 @Module({
   imports: [
     UserModule,
-    RoleModule
+    RoleModule,
+    NatsModule
   ],
   providers: [
     WebsocketNatsGateway,
-    NatsClient,
-    WebsocketTopicValidator,
-    AuthMiddleware
+    WebsocketTopicValidator
   ]
 })
 export class WebsocketModule {}
