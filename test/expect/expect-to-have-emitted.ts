6bbeca33bc74c1e0ff43835b5f75981b2f73a5ed
Kobe Kwanten <72790515+Kobe-Kwanten@users.noreply.github.com>
2025-04-15 09:29:47 +0200

    feat: allow multiple events in expect (#290)
    * feat: allow multiple events in expect
    
    * feat: remove test code

--- test/expect/expect-to-have-emitted.ts
+++ test/expect/expect-to-have-emitted.ts
@@ -5,22 +5,35 @@ import { DomainEventEmitter } from '../../src/modules/domain-events/domain-event
 
 export const toHaveEmitted: MatcherFunction<[DomainEvent]> = function (
   received: SinonStubbedInstance<DomainEventEmitter>,
-  event: DomainEvent
+  ...events: DomainEvent[]
 ): ExpectationResult {
-  const expectedEvent = expect.objectContaining({
+  const expectedEvents = events.map(event => expect.objectContaining({
     content: event.content,
     source: event.source,
     type: event.type,
     version: event.version
-  })
+  }))
 
   const calls = [...received.emit.getCalls(), ...received.emitOne.getCalls()]
-  const events = calls.flatMap(call => call?.firstArg as DomainEvent[] | DomainEvent | undefined)
-  const emitted = events?.some(event => this.equals(event, expectedEvent))
+  const originalEmits = calls.flatMap(
+    call => call?.firstArg as DomainEvent[] | DomainEvent | undefined
+  )
 
-  if (emitted) {
-    const stringifiedActual = this.utils.printReceived(events)
-    const stringifiedExpected = this.utils.printExpected(expectedEvent)
+  const emittedEvents = Array.from(originalEmits)
+  const foundEvents: unknown[] = []
+  for (const expectedEvent of expectedEvents) {
+    const emittedIndex = emittedEvents.findIndex(emit => this.equals(emit, expectedEvent))
+    if (emittedIndex >= 0) {
+      foundEvents.push(expectedEvent)
+      emittedEvents.splice(emittedIndex, 1)
+    }
+  }
+
+  const missingEvents = expectedEvents.filter(event => !foundEvents.some(e => e === event))
+
+  if (missingEvents.length === 0) {
+    const stringifiedActual = this.utils.printReceived(emittedEvents)
+    const stringifiedExpected = this.utils.printExpected(expectedEvents)
 
     return {
       pass: true,
@@ -29,22 +42,26 @@ export const toHaveEmitted: MatcherFunction<[DomainEvent]> = function (
   } else {
     return {
       pass: false,
-      message: () => '\n' + this.utils.printDiffOrStringify(
-        expectedEvent,
-        events.length === 1 ? events[0] : events,
-        'Expected event',
-        'Emitted event(s)',
-        false
-      )
+      message: () => {
+        return `\n Expected ${expectedEvents.length} event(s), missing ${missingEvents.length}`
+          + `\n Emitted ${originalEmits.length} event(s)`
+          + '\n' + this.utils.printDiffOrStringify(
+          missingEvents.length === 1 ? missingEvents[0] : missingEvents,
+          originalEmits.length === 1 ? originalEmits[0] : originalEmits,
+          'Expected event(s)',
+          'Emitted event(s)',
+          true
+        )
+      }
     }
   }
 }
 
 declare module 'expect' {
   interface AsymmetricMatchers {
-    toHaveEmitted: (event: DomainEvent) => ExpectationResult
+    toHaveEmitted: (...events: DomainEvent[]) => ExpectationResult
   }
   interface Matchers<R> {
-    toHaveEmitted: (event: DomainEvent) => R
+    toHaveEmitted: (...event: DomainEvent[]) => R
   }
 }
