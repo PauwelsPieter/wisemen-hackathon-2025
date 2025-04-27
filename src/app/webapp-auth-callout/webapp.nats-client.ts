809dff3e662de0031165735ecb72f8adc1c13748
Kobe Kwanten <72790515+Kobe-Kwanten@users.noreply.github.com>
2025-04-27 11:49:31 +0200

    change expected env keys (#315)

--- src/app/webapp-auth-callout/webapp.nats-client.ts
+++ src/app/webapp-auth-callout/webapp.nats-client.ts
@@ -3,7 +3,7 @@ import { NatsClient } from '../../modules/nats/nats-client.decorator.js'
 
 @NatsClient(configService => ({
   name: 'webapp-authenticator-client',
-  servers: [`${configService.getOrThrow('NATS_HOST')}:${configService.getOrThrow('NATS_PORT')}`],
+  servers: [`${configService.getOrThrow('WEBAPP_NATS_HOST')}:${configService.getOrThrow('WEBAPP_NATS_PORT')}`],
   authenticator: nkeyAuthenticator(new TextEncoder().encode(process.env.WEBAPP_NKEY ?? ''))
 }))
 export class WebappNatsClient {}
