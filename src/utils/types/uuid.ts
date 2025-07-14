91bb49ee3d346229a345270ece794de3aa491ccb
Kobe Kwanten <72790515+Kobe-Kwanten@users.noreply.github.com>
2025-07-14 11:56:50 +0200

    feat: add type support for nullable uuids (#377)

--- src/utils/types/uuid.ts
+++ src/utils/types/uuid.ts
@@ -2,6 +2,6 @@ export type Uuid<Brand extends string> = string
   & { readonly __brand: unique symbol }
   & { readonly __uuid: Brand }
 
-export function generateUuid<Brand extends Uuid<string>> (): Brand {
-  return crypto.randomUUID() as Brand
+export function generateUuid<Brand extends Uuid<string> | null> (): Exclude<Brand, null> {
+  return crypto.randomUUID() as Exclude<Brand, null>
 }
