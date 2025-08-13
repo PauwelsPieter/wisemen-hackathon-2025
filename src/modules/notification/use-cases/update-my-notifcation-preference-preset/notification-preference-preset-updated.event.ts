a12c0b969dba8537f84168a4fd32dcf17476f536
Kobe Kwanten <72790515+Kobe-Kwanten@users.noreply.github.com>
2025-08-13 18:05:14 +0200

    refactor: expect UserUuid in event instead of string (#399)

--- src/modules/notification/use-cases/update-my-notifcation-preference-preset/notification-preference-preset-updated.event.ts
+++ src/modules/notification/use-cases/update-my-notifcation-preference-preset/notification-preference-preset-updated.event.ts
@@ -5,16 +5,17 @@ import { DomainEventLog } from '../../../domain-event-log/domain-event-log.entit
 import { DomainEventType } from '../../../domain-events/domain-event-type.js'
 import { DomainEvent } from '../../../domain-events/domain-event.js'
 import { RegisterDomainEvent } from '../../../domain-events/register-domain-event.decorator.js'
+import { UserUuid } from '../../../../app/users/entities/user.uuid.js'
 
 @OneOfMeta(DomainEventLog, DomainEventType.NOTIFICATION_PREFERENCE_PRESET_UPDATED)
 export class NotificationPreferencePresetEventContent {
   @ApiProperty({ format: 'uuid' })
-  readonly userUuid: string
+  readonly userUuid: UserUuid
 
   @NotificationPresetApiProperty()
   readonly preset: NotificationPreset
 
-  constructor (userUuid: string, preset: NotificationPreset) {
+  constructor (userUuid: UserUuid, preset: NotificationPreset) {
     this.userUuid = userUuid
     this.preset = preset
   }
@@ -22,7 +23,7 @@ export class NotificationPreferencePresetEventContent {
 
 @RegisterDomainEvent(DomainEventType.NOTIFICATION_PREFERENCE_PRESET_UPDATED, 1)
 export class NotificationPreferencePresetUpdatedEvent extends DomainEvent {
-  constructor (userUuid: string, preset: NotificationPreset) {
+  constructor (userUuid: UserUuid, preset: NotificationPreset) {
     super({
       content: new NotificationPreferencePresetEventContent(userUuid, preset)
     })
